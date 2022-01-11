2069d72795863110703c80f7019468e97516dd32
Deb Kumar Mishra <47305904+Debkumar49@users.noreply.github.com>
2022-01-11 22:26:43 +0530

    Add files via upload
    Programs solved using IF Else Statement.

---
+++ Solutions(Python)/2. If Else Statement/alphabet.py
@@ -0,0 +1,7 @@
+c = input(" input , please : ")
+if c>='a' and c<='z':
+    print(f'{c} is an alphabet. ')
+elif c>='A' and c<='Z':
+    print(f'{c} is an Alphabet.')
+else:
+    print("The input is not an alphabet .")
