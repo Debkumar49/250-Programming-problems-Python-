1273cd476f0b851e73751908cd043eb7cee12ba7
Deb Kumar Mishra <47305904+Debkumar49@users.noreply.github.com>
2022-01-11 22:04:08 +0530

    Files uploaded
    Read Integer (N) and Print the First Three Powers (N^1, N^2, N^3)

---
+++ Solutions(Python)/1. Simple Questions/power.py
@@ -0,0 +1,7 @@
+def power():
+    n = int(input("Enter the Number you want to calculate : "))
+    print("The value of n^2 is ",pow(n,1))
+    print("The value of n^2 is ",pow(n,2))
+    print("The value of n^2 is ",pow(n,3))
+
+power()
